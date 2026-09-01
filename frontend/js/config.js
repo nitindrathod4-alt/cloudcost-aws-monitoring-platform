06ba1935b3eaffffc428326d42e819d6e411e8fe
Nitin Rathod <nitindrathod4@gmail.com>
2026-09-02 01:47:25 +0530

    Add browser-safe AWS API configuration

---
+++ frontend/js/config.js
@@ -0,0 +1,5 @@
+window.CLOUDCOST_CONFIG = Object.freeze({
+  // Set this to the API Gateway URL after `sam deploy`.
+  // Example: https://abc123.execute-api.ap-south-1.amazonaws.com/prod
+  API_BASE_URL: "",
+});
